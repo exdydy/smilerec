65ebe59c1271cba7c84b82c33808c56b229270b3
exdydy <37928970+exdydy@users.noreply.github.com>
2018-09-23 21:36:56 +0700

    full

---
+++ Smilecatcher/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Smilecatcher
+{
+    partial class Mainform
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.emojipicture = new System.Windows.Forms.PictureBox();
+            this.buttonMemorizeface = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonMemorizesmile = new System.Windows.Forms.Button();
+            this.buttonMemorizesad = new System.Windows.Forms.Button();
+            this.checkBoxface = new System.Windows.Forms.CheckBox();
+            this.checkBoxmouth = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emojipicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(0, 0);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(640, 360);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // emojipicture
+            // 
+            this.emojipicture.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.emojipicture.Location = new System.Drawing.Point(640, 0);
+            this.emojipicture.Name = "emojipicture";
+            this.emojipicture.Size = new System.Drawing.Size(256, 256);
+            this.emojipicture.TabIndex = 4;
+            this.emojipicture.TabStop = false;
+            // 
+            // buttonMemorizeface
+            // 
+            this.buttonMemorizeface.BackColor = System.Drawing.Color.PeachPuff;
+            this.buttonMemorizeface.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonMemorizeface.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonMemorizeface.Location = new System.Drawing.Point(0, 360);
+            this.buttonMemorizeface.Name = "buttonMemorizeface";
+            this.buttonMemorizeface.Size = new System.Drawing.Size(208, 128);
+            this.buttonMemorizeface.TabIndex = 6;
+            this.buttonMemorizeface.Text = "Memorize new face";
+            this.buttonMemorizeface.UseVisualStyleBackColor = false;
+            this.buttonMemorizeface.Click += new System.EventHandler(this.buttonMemorizeface_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(208, 376);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(432, 44);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "me";
+            // 
+            // buttonMemorizesmile
+            // 
+            this.buttonMemorizesmile.BackColor = System.Drawing.Color.PeachPuff;
+            this.buttonMemorizesmile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonMemorizesmile.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonMemorizesmile.Location = new System.Drawing.Point(640, 360);
+            this.buttonMemorizesmile.Name = "buttonMemorizesmile";
+            this.buttonMemorizesmile.Size = new System.Drawing.Size(256, 64);
+            this.buttonMemorizesmile.TabIndex = 8;
+            this.buttonMemorizesmile.Text = "Memorize new smile";
+            this.buttonMemorizesmile.UseVisualStyleBackColor = false;
+            this.buttonMemorizesmile.Click += new System.EventHandler(this.buttonMemorizesmile_Click);
+            // 
+            // buttonMemorizesad
+            // 
+            this.buttonMemorizesad.BackColor = System.Drawing.Color.PeachPuff;
+            this.buttonMemorizesad.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonMemorizesad.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonMemorizesad.Location = new System.Drawing.Point(640, 424);
+            this.buttonMemorizesad.Name = "buttonMemorizesad";
+            this.buttonMemorizesad.Size = new System.Drawing.Size(256, 64);
+            this.buttonMemorizesad.TabIndex = 9;
+            this.buttonMemorizesad.Text = "Memorize new sad";
+            this.buttonMemorizesad.UseVisualStyleBackColor = false;
+            this.buttonMemorizesad.Click += new System.EventHandler(this.buttonMemorizesad_Click);
+            // 
+            // checkBoxface
+            // 
+            this.checkBoxface.AutoSize = true;
+            this.checkBoxface.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBoxface.Location = new System.Drawing.Point(216, 440);
+            this.checkBoxface.Name = "checkBoxface";
+            this.checkBoxface.Size = new System.Drawing.Size(174, 28);
+            this.checkBoxface.TabIndex = 10;
+            this.checkBoxface.Text = "Show face boxes";
+            this.checkBoxface.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxmouth
+            // 
+            this.checkBoxmouth.AutoSize = true;
+            this.checkBoxmouth.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBoxmouth.Location = new System.Drawing.Point(432, 440);
+            this.checkBoxmouth.Name = "checkBoxmouth";
+            this.checkBoxmouth.Size = new System.Drawing.Size(192, 28);
+            this.checkBoxmouth.TabIndex = 11;
+            this.checkBoxmouth.Text = "Show mouth boxes";
+            this.checkBoxmouth.UseVisualStyleBackColor = true;
+            // 
+            // Mainform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Bisque;
+            this.ClientSize = new System.Drawing.Size(896, 490);
+            this.Controls.Add(this.checkBoxmouth);
+            this.Controls.Add(this.checkBoxface);
+            this.Controls.Add(this.buttonMemorizesad);
+            this.Controls.Add(this.buttonMemorizesmile);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonMemorizeface);
+            this.Controls.Add(this.emojipicture);
+            this.Controls.Add(this.imageBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Mainform";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main";
+            this.Load += new System.EventHandler(this.Mainform_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emojipicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.PictureBox emojipicture;
+        private System.Windows.Forms.Button buttonMemorizeface;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonMemorizesmile;
+        private System.Windows.Forms.Button buttonMemorizesad;
+        private System.Windows.Forms.CheckBox checkBoxface;
+        private System.Windows.Forms.CheckBox checkBoxmouth;
+    }
+}
+
---
+++ Smilecatcher/Form1.cs
@@ -0,0 +1,179 @@
+﻿
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+
+
+
+namespace Smilecatcher
+{
+    public partial class Mainform : Form
+    {
+        HaarCascade facedetector;
+        HaarCascade mouthdetector;
+        MCvTermCriteria faceCriteria;
+        MCvTermCriteria mouthCriteria;
+        double criteria = 0.001;
+        Image smileimage;
+        Image sadimage;
+        Image<Bgr, byte> frame;
+        Capture camera;
+        Image<Gray, byte> result;
+        Image<Gray, byte> grayface = null;
+        Image<Gray, byte> graymouth = null;
+        List<Image<Gray, byte>> trainedfaces = new List<Image<Gray, byte>>();
+        List<Image<Gray, byte>> trainedmouths = new List<Image<Gray, byte>>();
+        List<string> labelsface = new List<string>();
+        List<string> labelsmouth = new List<string>();
+        int countfaces = 0, countmouths = 0;
+        MCvAvgComp[][] facesDetectedNow;
+        MCvAvgComp[][] mouthsDetectedNow;
+
+        MCvFont font = new MCvFont(Emgu.CV.CvEnum.FONT.CV_FONT_HERSHEY_TRIPLEX, 0.6d, 0.6d);
+
+        public Mainform()
+        {
+            InitializeComponent();
+
+            
+            facedetector = new HaarCascade(Application.StartupPath + "/data/xml/haarcascade_frontalface_default.xml");
+            mouthdetector = new HaarCascade(Application.StartupPath + "/data/xml/haarcascade_mcs_mouth.xml");
+            try
+            {
+                countfaces = Directory.GetFiles(Application.StartupPath + "/data/faces/").Length;
+                countmouths = Directory.GetFiles(Application.StartupPath + "/data/smiles/").Length + Directory.GetFiles(Application.StartupPath + "/data/sads/").Length;
+
+                foreach (string file in Directory.EnumerateFiles(Application.StartupPath + "/data/faces/", "*.bmp"))
+                {
+                    trainedfaces.Add(new Image<Gray, byte>(file));
+                    labelsface.Add(Path.GetFileNameWithoutExtension(file));
+                }
+
+                foreach (string file in Directory.EnumerateFiles(Application.StartupPath + "/data/smiles/", "*.bmp"))
+                {
+                    trainedmouths.Add(new Image<Gray, byte>(file));
+                    labelsmouth.Add("smile");
+                }
+                foreach (string file in Directory.EnumerateFiles(Application.StartupPath + "/data/sads/", "*.bmp"))
+                {
+                    trainedmouths.Add(new Image<Gray, byte>(file));
+                    labelsmouth.Add("sad");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Looks like smth went wrong trying to read data files");
+            }
+
+            smileimage = Image.FromFile(Application.StartupPath + "/data/smile.png");
+            sadimage = Image.FromFile(Application.StartupPath + "/data/sad.png");
+            emojipicture.Image = smileimage;
+           }
+
+        private void FrameProcessing(object sender, EventArgs e)
+        {
+            frame = camera.QueryFrame().Resize(640, 360, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+            grayface = frame.Convert<Gray, Byte>();
+            facesDetectedNow = grayface.DetectHaarCascade(facedetector, 1.2, 10, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(100, 100));
+            graymouth = frame.Convert<Gray, Byte>();
+            mouthsDetectedNow = graymouth.DetectHaarCascade(mouthdetector, 1.2, 10, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(100, 50));
+            foreach (MCvAvgComp f in facesDetectedNow[0])
+            {
+                result = frame.Copy(f.rect).Convert<Gray, Byte>().Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+                if (checkBoxface.Checked) frame.Draw(f.rect, new Bgr(Color.Green), 3);
+                if(trainedfaces.ToArray().Length !=0)
+                {
+                    faceCriteria = new MCvTermCriteria(countfaces, criteria);
+                    EigenObjectRecognizer recognizer = new EigenObjectRecognizer(trainedfaces.ToArray(), labelsface.ToArray(), 1500, ref faceCriteria);
+                    frame.Draw(recognizer.Recognize(result), ref font, new Point(f.rect.X - 2, f.rect.Y - 2), new Bgr(Color.Red));
+                }
+            }
+            foreach (MCvAvgComp m in mouthsDetectedNow[0])
+            {
+                result = frame.Copy(m.rect).Convert<Gray, Byte>().Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+                if (checkBoxmouth.Checked)  frame.Draw(m.rect, new Bgr(Color.Blue), 3);
+
+                if (trainedmouths.ToArray().Length != 0)
+                {
+                    mouthCriteria = new MCvTermCriteria(countmouths, criteria);
+                    EigenObjectRecognizer recognizer = new EigenObjectRecognizer(trainedmouths.ToArray(), labelsmouth.ToArray(), 1500, ref mouthCriteria);
+                    if (recognizer.Recognize(result) == "sad")
+                    {
+                        emojipicture.Image = sadimage;
+                    }
+                    else
+                    {
+                        emojipicture.Image = smileimage;
+                    }
+                }
+            }
+            imageBox1.Image = frame;
+        }
+
+        private void Mainform_Load(object sender, EventArgs e)
+        {
+            camera = new Capture();
+            camera.QueryFrame();
+            Application.Idle += new EventHandler(FrameProcessing);
+        }
+
+        private void buttonMemorizesmile_Click(object sender, EventArgs e)
+        {
+            if (mouthsDetectedNow[0].Length == 1)
+            {
+                trainedmouths.Add(frame.Copy(mouthsDetectedNow[0][0].rect).Convert<Gray, byte>().Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC));
+                labelsmouth.Add("smile");
+                trainedmouths.ToArray()[countmouths].Save(Application.StartupPath + "/data/smiles/" + Directory.GetFiles(Application.StartupPath + "/data/smiles/").Length + ".bmp");
+                countmouths++;
+                MessageBox.Show("This smile Added Successfully"); 
+            }
+            else
+            {
+                MessageBox.Show("Detected no mouth / more than one mouth");
+            }
+        }
+
+        private void buttonMemorizesad_Click(object sender, EventArgs e)
+        {
+            if (mouthsDetectedNow[0].Length == 1)
+            {
+                trainedmouths.Add(frame.Copy(mouthsDetectedNow[0][0].rect).Convert<Gray, byte>().Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC));
+                labelsmouth.Add("sad");
+                trainedmouths.ToArray()[countmouths].Save(Application.StartupPath + "/data/sads/" + Directory.GetFiles(Application.StartupPath + "/data/sads/").Length + ".bmp");
+                countmouths++;
+                MessageBox.Show("This sad Added Successfully");
+            }
+            else
+            {
+                MessageBox.Show("Detected no mouth / more than one mouth");
+            }
+        }
+
+        private void buttonMemorizeface_Click(object sender, EventArgs e)
+        {
+            if (facesDetectedNow[0].Length == 1)
+            {
+                trainedfaces.Add(frame.Copy(facesDetectedNow[0][0].rect).Convert<Gray, byte>().Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC));
+                labelsface.Add(textBox1.Text);
+                trainedfaces.ToArray()[countfaces].Save(Application.StartupPath + "/data/faces/" + textBox1.Text + ".bmp");
+                File.AppendAllText(Application.StartupPath + "/data/Faces.txt", labelsface.ToArray()[countfaces] + ",");
+                countfaces++;
+                MessageBox.Show(textBox1.Text + " Added Successfully");
+            }
+               else
+            {
+                MessageBox.Show("Detected no faces / more than one face");
+            }
+        }
+    }
+}
